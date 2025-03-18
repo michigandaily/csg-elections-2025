c5609f70053db98c42c4af7052c053d45d9fac39
meijja <juliamei@umich.edu>
2025-03-18 14:03:44 -0400

    Added Year in Review block

--- src/js/Info.tsx
+++ src/js/Info.tsx
@@ -1,16 +1,16 @@
 import Ballot from "./Ballot"
+import Review from "./Review"
 import '../css/info.scss'
 
 const Info = () => {
     return (
-        <div className="info-container">
-            {/* Flexbox containing ballot (left) and article (right) */}
-            <Ballot />
-            <div className="main-article-container">
-        
-            </div>
-        </div>
-    )
+			<div className="info-container">
+				{/* Flexbox containing ballot (left) and article (right) */}
+				<Ballot />
+				<Review />
+				<div className="main-article-container"></div>
+			</div>
+		);
 }
 
 export default Info;
---
+++ src/js/Review.tsx
@@ -0,0 +1,20 @@
+import '../css/info.scss';
+
+const Review = () => {
+	return (
+		<div className="review-container">
+			{/* TODO: replace with actual CSG pic */}
+			<img src="src/assets/placeholder.png" alt="CSG"></img>
+			<h1>CSG: A Year in Review</h1>
+			<p>
+				Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+				Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+				Blah Blah Blah Blah Blah Blah ah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+				Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+				Blah Blah ah Blah Blah Blah Blah Blah Blah
+			</p>
+		</div>
+	);
+};
+
+export default Review;
