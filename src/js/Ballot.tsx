fb89b562564c8b705665c9ef1daed1dfed220928
Emily Chen <emychen@umich.edu>
2025-03-24 15:45:53 -0400

    add url attribute to ballot json

--- src/js/Ballot.tsx
+++ src/js/Ballot.tsx
@@ -8,43 +8,66 @@ const Ballot = () => {
 			<h3>Executive</h3>
 			<div className="candidates">
 				{candidates.executive.map((candidate) => {
-					const [name, party] = candidate.split(' - ');
+					const name = candidate.candidate;
+					const url = candidate.url;
+					const party = candidate.party;
 
 					return (
-						<p key={candidate}>
-							☐ {name}
-							{party && <strong> - {party}</strong>}
-						</p>
+						<div key={name}>
+							<p className="candidate-name">☐ {url !== '' ? <a href={url}>{name}</a> : name}</p>
+							<p className="candidate-party">{party}</p>
+						</div>
 					);
 				})}
 			</div>
-			<h3>DPSS Board Representative</h3>
+			<h3>
+				<a href="https://www.michigandaily.com/news/student-government/meet-the-candidates-qa-with-candidates-for-umpd-oversight-committee/">
+					DPSS Board Representative
+				</a>
+			</h3>
 			<div className="candidates">
 				{candidates.dpss.map((candidate) => {
-					const [name, party] = candidate.split(' - ');
+					const name = candidate.candidate;
+					const url = candidate.url;
+					const party = candidate.party;
 
 					return (
-						<p key={candidate}>
-							☐ {name}
-							{party && <strong> - {party}</strong>}
-						</p>
+						<div key={name}>
+							<p className="candidate-name">☐ {url !== '' ? <a href={url}>{name}</a> : name}</p>
+							<p className="candidate-party">{party}</p>
+						</div>
 					);
 				})}
 			</div>
-			<h3>Constitutional Amendments</h3>
+
+			<h3>
+				<a href="https://www.michigandaily.com/news/student-government/a-students-guide-to-the-2025-csg-constitutional-amendments/">
+					Constitutional Amendments
+				</a>
+			</h3>
+
 			<div className="candidates">
 				{candidates.amendments.map((candidate) => {
-					return <p key={candidate}>☐ {candidate}</p>;
+					const text = candidate.candidate;
+					const url = candidate.url;
+
+					return (
+						<p key={text} className="candidate-name">
+							☐ {url !== '' ? <a href={url}>{text}</a> : text}
+						</p>
+					);
 				})}
 			</div>
 			<h3>Elected representatives</h3>
-			<p>
-				Varies by school / college. Click{' '}
-				<a href="https://docs.google.com/spreadsheets/d/1SrA8hGbhwkzDQrAKSX2jpYbeRVnXCpsNx0aMs9UQxNE/edit?gid=0#gid=0">
-					here
-				</a>{' '}
-				to learn more.
-			</p>
+			<div className="candidates">
+				<p className="candidate-name">
+					Varies by school / college. Click{' '}
+					<a href="https://docs.google.com/spreadsheets/d/1SrA8hGbhwkzDQrAKSX2jpYbeRVnXCpsNx0aMs9UQxNE/edit?gid=0#gid=0">
+						here
+					</a>{' '}
+					to learn more.
+				</p>
+			</div>
 		</div>
 	);
 };
